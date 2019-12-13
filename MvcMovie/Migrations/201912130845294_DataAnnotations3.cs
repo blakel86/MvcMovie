cbdd0b813be54c553b4f64b9f7f544d1cbc83356
Blake Laroux <blake.laroux@rac.com.au>
2019-12-13 16:57:05 +0800

    MVCMovie - completed tutorial

---
+++ MvcMovie/Migrations/201912130845294_DataAnnotations3.cs
@@ -0,0 +1,16 @@
+﻿namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotations3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
